6a4adf3cfa9e7c9bb2f70443c67cf18e86ee6fe3
ohubabs <obakinolugbade@gmail.com>
2024-03-29 12:20:41 -0700

    terraform provider credentials

---
+++ provider.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-west-2"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "/home/terra/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+    config_path = "/home/terra/.kube/config"
+  }
