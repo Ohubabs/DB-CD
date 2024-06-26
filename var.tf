6b6b024bf5598f781fcb064795707a55e9aa38c2
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-06-25 19:40:37 -0700

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,9 +1,9 @@
 variable "krillin" {
     description = "Username for springapp mongo db access"
-    default = "ohubabs" #Always makes sure this matches the root user or custom user for the mongo database in the mongo helm chart
+    default = "" #Always makes sure this matches the root user or custom user for the mongo database in the mongo helm chart
 }
 
 variable "destructodisk" {
     description = "Password for springapp mongo db access"
-    default = "SpiderVerse" 
+    default = "" 
 }
